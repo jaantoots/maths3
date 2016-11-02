f3433365c3852cf16ebaf7cc005b1f55bcf33d7d
Jaan Toots <jaantoots@gmail.com>
2016-11-02 15:06:18 +0000

    Build website 03d072a32857617ed67efc90002fe7ea7c1046d3

---
+++ get_examples.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+data = File.read('_data.json')
+courses = JSON.parse(data)
+
+courses.each do |dir, course|
+  if course['notes']
+    `wget -nv -N -P _local/#{dir} #{course['notes']}`
+  end
+  if course['sheets']
+    course['sheets'].each do |_, sheet|
+      `wget -nv -N -P _local/#{dir} #{sheet['url']}`
+      if sheet['sol']
+        `wget -nv -N -P _local/#{dir} #{sheet['sol']}`
+      end
+    end
+  end
+end
