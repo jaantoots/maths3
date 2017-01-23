48fab8774fc46b680beeb2848f3c29624edbe1b4
Jaan Toots <jaantoots@gmail.com>
2017-01-23 17:54:30 +0000

    Update get_examples script to download altnotes

--- get_examples.rb
+++ get_examples.rb
@@ -9,6 +9,11 @@ courses.each do |dir, course|
   if course['notes']
     `wget -nv -N -P _local/#{dir} #{course['notes']}`
   end
+  if course['altnotes']
+    course['altnotes'].each do |_, url|
+      `wget -nv -N -P _local/#{dir} #{url}`
+    end
+  end
   if course['sheets']
     course['sheets'].each do |_, sheet|
       `wget -nv -N -P _local/#{dir} #{sheet['url']}`
